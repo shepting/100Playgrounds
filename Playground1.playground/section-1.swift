28ec078a1801dffac551568595b4c09596a819e8
Steven Hepting <shepting@gmail.com>
2015-03-23 13:36:56 -0700

    First project. Fibonacci. Of course.

--- Playground1.playground/section-1.swift
+++ Playground1.playground/section-1.swift
@@ -1,5 +1,12 @@
 // Playground - noun: a place where people can play
 
-import UIKit
+func fibonacci(number: Int) -> Int {
+    if (number == 0 || number == 1) {
+        return 1
+    } else {
+        return fibonacci(number - 1) + fibonacci(number - 2)
+    }
+}
 
-var str = "Hello, playground"
+fibonacci(4)
+fibonacci(8)
