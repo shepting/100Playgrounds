a19dec8954bc080d52eaa50a481ec872d222eab3
Steven Hepting <shepting@gmail.com>
2015-03-24 17:37:24 -0700

    Add Star Wars fetching.

--- Playground2.playground/section-1.swift
+++ Playground2.playground/section-1.swift
@@ -2,4 +2,19 @@
 
 import UIKit
 
-var str = "Hello, playground"
+if let
+    url      = NSURL(string: "http://swapi.co/api/people"),
+    data     = NSData(contentsOfURL: url),
+    userList: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) {
+        //    userList = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? Dictionary<String, [AnyObject]> {
+        println(userList["results"])
+        if let userDicts: AnyObject? = userList["results"] {
+            userDicts
+            println(userDicts)
+            println(userDicts!)
+            
+            let users: AnyObject? = userDicts?[0]
+            users?.allKeys
+            users?["name"]
+        }
+}
