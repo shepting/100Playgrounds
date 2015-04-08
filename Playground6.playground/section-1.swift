27f180076443e09a4ec993a40f9142d7d7399c0a
Steven Hepting <shepting@gmail.com>
2015-04-08 11:37:07 -0700

    Added new linked list node Playground.

--- Playground6.playground/section-1.swift
+++ Playground6.playground/section-1.swift
@@ -1,5 +1,28 @@
 // Playground - noun: a place where people can play
 
+// Simple linked list node
+
 import UIKit
 
-var str = "Hello, playground"
+@objc class Node {
+    var next: Node?
+    var data: String = ""
+    
+    init(_ data: String, next: Node? = nil) {
+        self.next = next
+        self.data = data
+    }
+    
+    func debugQuickLookObject() -> AnyObject? {
+        if let next = next {
+            return "next->Node(\'\(next.data)')\t\tdata: \"\(data)\""
+        } else {
+            return "Next: nil \t\t\t\tdata:\"\(data)\""
+        }
+    }
+}
+
+let three = Node("three")
+let two = Node("two", next: three)
+let one = Node("one", next: two)
+var head = Node("head", next: one)
